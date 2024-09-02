ccf59dc5c9a166d658832115f6d06ce5695591d9
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-09-02 18:14:28 +0530

    the Id field change long to string and data added

--- src/main/java/com/practice/entity/JournalEntry.java
+++ src/main/java/com/practice/entity/JournalEntry.java
@@ -1,27 +1,48 @@
 package com.practice.entity;
 
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collation = "journal_entries")
 public class JournalEntry {
-	private long id;
+	@Id
+	private String id;
 	private String title;
 	private String content;
-	
-	public long getId() {
+	private Date date;
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getId() {
 		return id;
 	}
-	public void setId(long id) {
+
+	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getContent() {
 		return content;
 	}
+
 	public void setContent(String content) {
 		this.content = content;
 	}
-	
+
 }
