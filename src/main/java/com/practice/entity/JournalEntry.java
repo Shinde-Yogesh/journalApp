e43ba700d2f3d5a3c88fde1d97dd1e8ba3309db3
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-09-05 18:12:27 +0530

    Adding lombok dependancy for the reduces the line of code Getter and setter auto generated in compile time

--- src/main/java/com/practice/entity/JournalEntry.java
+++ src/main/java/com/practice/entity/JournalEntry.java
@@ -6,44 +6,16 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
 @Document(collection  = "journal_entries")
+@Data
 public class JournalEntry {
 	@Id
 	private ObjectId id;
 	private String title;
 	private String content;
 	private LocalDateTime date;
-
-	public LocalDateTime getDate() {
-		return date;
-	}
-
-	public void setDate(LocalDateTime localDateTime) {
-		this.date = localDateTime;
-	}
-
-	public ObjectId getId() {
-		return id;
-	}
-
-	public void setId(ObjectId id) {
-		this.id = id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
-	}
-
 }
