41806cd892c550df34d7f77fe38c38f349a0c53d
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-09-05 19:30:13 +0530

    user entity created for the indisual entries

--- src/main/java/com/practice/entity/JournalEntry.java
+++ src/main/java/com/practice/entity/JournalEntry.java
@@ -6,15 +6,16 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import com.mongodb.lang.NonNull;
 
-@Document(collection  = "journal_entries")
+import lombok.Data;
+
+@Document(collection = "journal_entries")
 @Data
 public class JournalEntry {
 	@Id
 	private ObjectId id;
+	@NonNull
 	private String title;
 	private String content;
 	private LocalDateTime date;
