e807e20cdc91c7b68aac9991c1cfbeee285df6c5
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-09-06 17:21:31 +0530

    the user connected to the journal entries means only id show in collection in journal entries

--- src/main/java/com/practice/service/JournalEntryService.java
+++ src/main/java/com/practice/service/JournalEntryService.java
@@ -1,5 +1,6 @@
 package com.practice.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.practice.entity.JournalEntry;
+import com.practice.entity.User;
 import com.practice.repository.JournalEntryRepository;
 
 @Service
@@ -15,9 +17,16 @@ public class JournalEntryService {
 
 	@Autowired
 	private JournalEntryRepository journalEntryRepository;
+	
+	@Autowired UserService userService;
 
-	public void saveEntry(JournalEntry journalEntry) {
-		journalEntryRepository.save(journalEntry);
+	public void saveEntry(JournalEntry journalEntry, String userName) {
+		
+		journalEntry.setDate(LocalDateTime.now());
+		User user = userService.findByUserName(userName);
+		JournalEntry saved = journalEntryRepository.save(journalEntry);
+		user.getJournalEntries().add(saved);
+		userService.saveEntry(user);
 	}
 	
 	public List<JournalEntry> getAll()
