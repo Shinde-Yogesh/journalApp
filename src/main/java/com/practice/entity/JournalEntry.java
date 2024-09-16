1d625a78f004ee3f3b52c4af5db30476dd9d8a64
Yogesh Shinde <yogeshshinde2324@gmail.com>
2024-09-16 20:37:31 +0530

    Update JournalEntry.java

--- src/main/java/com/practice/entity/JournalEntry.java
+++ src/main/java/com/practice/entity/JournalEntry.java
@@ -1,13 +1,10 @@
 package com.practice.entity;
 
 import java.time.LocalDateTime;
-
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 import com.mongodb.lang.NonNull;
-
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
