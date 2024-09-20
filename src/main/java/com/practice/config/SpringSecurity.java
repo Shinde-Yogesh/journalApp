ec57c3abc4cc3ac2df91922e711a10dad4feb8ef
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-09-20 16:21:58 +0530

    config class created with package

---
+++ src/main/java/com/practice/config/SpringSecurity.java
@@ -0,0 +1,10 @@
+package com.practice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+class SpringSecurity {
+
+}
