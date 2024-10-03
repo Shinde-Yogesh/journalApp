15be7730bdaf54815da49907973d7e4394d38697
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-10-03 21:38:43 +0530

    Unit testing

--- src/main/java/com/practice/repository/UserRepositoryImpl.java
+++ src/main/java/com/practice/repository/UserRepositoryImpl.java
@@ -1,12 +1,26 @@
 package com.practice.repository;
 
 import com.practice.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Component;
 
+import java.util.List;
+@Component
 public class UserRepositoryImpl {
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
-    
-    public List<User> getUserForSA()
-    {
+    public List<User> getUserForSA() {
+        Query query = new Query();
 
+        query.addCriteria(Criteria.where("").regex("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\\\.[A-Z|a-z]{2,6}$"));
+
+        query.addCriteria(Criteria.where("email").exists(true));
+        query.addCriteria(Criteria.where("sentimentAnalysis").is(true));
+        List<User> users = mongoTemplate.find(query, User.class);
+        return users;
     }
 }
