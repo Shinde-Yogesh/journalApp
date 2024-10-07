081741fb2f980c9d2dd6f6d33a1eb94b089a7cd7
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-10-07 17:14:33 +0530

    class created for the schedular

---
+++ src/main/java/com/practice/schedular/UserSchedular.java
@@ -0,0 +1,4 @@
+package com.practice.schedular;
+
+public class UserSchedular {
+}
---
+++ src/main/java/com/practice/service/SentimentAnalysisService.java
@@ -0,0 +1,8 @@
+package com.practice.service;
+
+public class SentimentAnalysisService {
+    public int getSentiment(String text)
+    {
+        return 1;
+    }
+}
