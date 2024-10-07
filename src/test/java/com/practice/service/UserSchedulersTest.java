df4611408e84082dc31bde1b798088e9e0905b21
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-10-07 21:58:57 +0530

    functionality added for the emotion tracker and sending mail

--- src/test/java/com/practice/service/UserSchedulersTest.java
+++ src/test/java/com/practice/service/UserSchedulersTest.java
@@ -1,4 +1,19 @@
 package com.practice.service;
 
+import com.practice.schedular.UserSchedular;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class UserSchedulersTest {
+
+    @Autowired
+    private UserSchedular userSchedular;
+
+    @Test
+    public void testFetchUsersAndSendSaMail()
+    {
+        userSchedular.fetchUsersAndSendSaMail();
+    }
 }
