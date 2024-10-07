228ae34b90436132312950ee7a89c5827d3a2522
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-10-07 18:05:37 +0530

    class ctreated for the app cache store

--- src/main/java/com/practice/cache/AppCache.java
+++ src/main/java/com/practice/cache/AppCache.java
@@ -1,4 +1,28 @@
 package com.practice.cache;
 
+import com.practice.entity.ConfigJournalAppEntity;
+import com.practice.repository.ConfigJournalAppRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
 public class AppCache {
+    @Autowired
+    private ConfigJournalAppRepository configJournalAppRepository;
+
+    public Map<String, String> appCache;
+
+    @PostConstruct
+    public void init(){
+        appCache = new HashMap<>();
+        List<ConfigJournalAppEntity> all = configJournalAppRepository.findAll();
+        for (ConfigJournalAppEntity configJournalAppEntity : all) {
+            appCache.put(configJournalAppEntity.getKey(), configJournalAppEntity.getValue());
+        }
+    }
 }
