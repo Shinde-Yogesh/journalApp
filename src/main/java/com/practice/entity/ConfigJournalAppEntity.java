b01ed945677d27299c7d3cbc72bd75c2ca70e955
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-10-07 18:08:46 +0530

    class created for the get key and value

--- src/main/java/com/practice/entity/ConfigJournalAppEntity.java
+++ src/main/java/com/practice/entity/ConfigJournalAppEntity.java
@@ -1,4 +1,14 @@
 package com.practice.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "config_journal_app")
+@Data
+@NoArgsConstructor
 public class ConfigJournalAppEntity {
+    private String key;
+
+    private String value;
 }
