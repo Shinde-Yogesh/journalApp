57648c78154051b29d81563382e21c5acd5cec15
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-10-07 17:37:02 +0530

    the code for the filter of the sentiment and send to the appropriate user

--- src/main/java/com/practice/schedular/UserSchedular.java
+++ src/main/java/com/practice/schedular/UserSchedular.java
@@ -1,4 +1,40 @@
 package com.practice.schedular;
 
+import com.practice.entity.JournalEntry;
+import com.practice.entity.User;
+import com.practice.repository.UserRepositoryImpl;
+import com.practice.service.EmailService;
+import com.practice.service.SentimentAnalysisService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
 public class UserSchedular {
+
+    @Autowired
+    private EmailService emailService;
+
+    @Autowired
+    private UserRepositoryImpl userRepository;
+
+    @Autowired
+    private SentimentAnalysisService sentimentAnalysisService;
+
+    public void fetchUsersAndSendSaMail()
+    {
+        List<User> users = userRepository.getUserForSA();
+        for(User user: users)
+        {
+            List<JournalEntry> journalEntries = user.getJournalEntries();
+            List<String> filteredEntries = journalEntries.stream().filter(x -> x.getDate().isAfter(LocalDateTime.now().minus(7, ChronoUnit.DAYS))).map(x->x.getContent()).collect(Collectors.toList());
+            String entry = String.join(" ", filteredEntries);
+            String sentiment = sentimentAnalysisService.getSentiment(entry);
+            emailService.sendEmail(user.getEmail(),"Sentiment for last 7 Day's",sentiment);
+        }
+    }
 }
