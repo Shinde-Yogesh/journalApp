f6255dd0fc85e8bf0809a5f1c36d481bc6a4199b
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-10-08 18:53:27 +0530

    test created for the communication

--- src/test/java/com/practice/service/RedisTests.java
+++ src/test/java/com/practice/service/RedisTests.java
@@ -1,4 +1,21 @@
 package com.practice.service;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@SpringBootTest
 public class RedisTests {
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    void testSendMail()
+    {
+        redisTemplate.opsForValue().set("email","yogeshshinde2324@gmail.com");
+        Object email = redisTemplate.opsForValue().get("email");
+        int a= 1;
+    }
 }
