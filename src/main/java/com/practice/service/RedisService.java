f1b205650c775e9a97967c4413b94e254c77c539
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-10-08 23:29:59 +0530

    service for get the key and value for configuration in redis

--- src/main/java/com/practice/service/RedisService.java
+++ src/main/java/com/practice/service/RedisService.java
@@ -1,4 +1,40 @@
 package com.practice.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+@Slf4j
 public class RedisService {
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    public <T> T get(String key, Class<T> entityClass) {
+        try {
+            Object value = redisTemplate.opsForValue().get(key);
+            if (value != null) {
+                return objectMapper.readValue(value.toString(), entityClass);
+            }
+        } catch (Exception e) {
+            log.error("Exception while fetching from Redis: ", e);
+        }
+        return null;
+    }
+
+    public void set(String key, Object value, Long ttl) {
+        try {
+            String jsonValue = objectMapper.writeValueAsString(value);
+            redisTemplate.opsForValue().set(key, jsonValue, ttl, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            log.error("Exception while setting Redis value: ", e);
+        }
+    }
 }
